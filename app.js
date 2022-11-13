20c885617c97bb8fbe6b3a56572e2721436311d6
Pony_Yuhan <89634392+ponyyuhan@users.noreply.github.com>
2022-11-13 20:19:16 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+let Koa = require('koa');
+let app = new Koa();
+let Router = require('koa-router');
+const koaBody = require('koa-body');
+let router = new Router;
+
+app.use(koaBody({multipart: true}));
+
+let log4js = require('log4js');
+log4js.configure('./log4js.json');
+log4js.level = 'DEBUG';
+let logger = log4js.getLogger('app');
+
+function getServerInfo() {
+    let os = require("os");
+    let ifaces = os.networkInterfaces();
+    let adresses = Object.keys(ifaces).reduce(function (result, dev) {
+        return result.concat(ifaces[dev].reduce(function (result, details) {
+            return result.concat(details.family === 'IPv4' && !details.internal ? [details.address] : []);
+        }, []));
+    });
+    return `IP ${adresses}, hostname: ${os.hostname()}`
+}
+
+router.all('/', async ctx =>{
+    logger.info('this is an index page')
+    ctx.body = `Yuhan Ma index page \n\n${getServerInfo()}\n`
+});
+
+router.all('/hello/:name', async ctx =>{
+    let name = ctx.params.name
+    logger.info('it is a hello page')
+    ctx.body = `Hello, this is Yuhan Ma. and  your ${name ? name : 'world'} \n\n${getServerInfo()}\n`
+});
+
+app.use(router.routes());
+
+let port = process.env.PORT || 8080;
+try{
+    app.listen(port);
+    logger.info('Server started successfully and listened on '+ port +'\n'+'http://localhost:'+port);
+}catch(err){
+    console.error(err);
+}
